998151cbc2f68e32075f845de4d332c2e4d50222
Andrea99F <39659414+Andrea99F@users.noreply.github.com>
2018-08-22 17:04:46 +0200

    a

--- execute.php
+++ execute.php
@@ -30,8 +30,8 @@ $date = isset($message['date']) ? $message['date'] : "";
 $text = isset($message['text']) ? $message['text'] : "";
 $text = trim($text);
 
-if (($text==="/help") ||($text==="/start")){
-	$messaggio = "write /help to know what I can do \n write \report + a text to send a message to developers \n write /info to know what bot recive"
+if ($text==="/start"){
+	$messaggio = "write /start to know what I can do \n write \report + a text to send a message to developers \n write /info to know what bot recive"
 	sendmess($chatId, $messaggio);
 }
 if ($text==="/info"){
