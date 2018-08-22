f220797865d4d1a70d8972e66e35dd40b512f3d7
Andrea99F <39659414+Andrea99F@users.noreply.github.com>
2018-08-22 16:25:49 +0200

    a

--- execute.php
+++ execute.php
@@ -19,25 +19,19 @@ function sendmess($chat,$testo)
 
 //inizio programma
 
-
-
-
-
 $message = isset($update['message']) ? $update['message'] : "";
 $messageId = isset($message['message_id']) ? $message['message_id'] : "";
 $chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
 $userId = isset($message['from']['id']) ? $message['from']['id'] : "";
-$firstname = isset($message['chat']['first_name']) ? $message['chat']['first_name'] : "";
-$lastname = isset($message['chat']['last_name']) ? $message['chat']['last_name'] : "";
-$username = isset($message['chat']['username']) ? $message['chat']['username'] : "";
+$firstname = isset($message['from']['first_name']) ? $message['from']['first_name'] : "";
+$lastname = isset($message['from']['last_name']) ? $message['from']['last_name'] : "";
+$username = isset($message['from']['username']) ? $message['from']['username'] : "";
 $date = isset($message['date']) ? $message['date'] : "";
 $text = isset($message['text']) ? $message['text'] : "";
 $text = trim($text);
 
 if ($text==="/info"){
 	$messaggio = json_encode($message, JSON_PRETTY_PRINT);
-
 	sendmess($chatId, $messaggio);
-	
 }
 
